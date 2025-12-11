3ec32f02b4a9fef271fce560d94c61ff982e4988
DalPra0 <153088537+DalPra0@users.noreply.github.com>
2025-12-11 15:51:41 -0300

    Initialize AmigoSecretoProject SwiftUI app
    Set up a new SwiftUI iOS project with basic structure, including main app entry point, ContentView, and default asset catalogs. Project configuration files for Xcode are included.

---
+++ AmigoSecretoProject/AmigoSecretoProject/AmigoSecretoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmigoSecretoProjectApp.swift
+//  AmigoSecretoProject
+//
+//  Created by Lucas Dal Pra Brascher on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct AmigoSecretoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
